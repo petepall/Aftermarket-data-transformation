8a844a74dfbd47f70391bd85cae19b9caea88233
Peter Pallen <42099752+petepall@users.noreply.github.com>
2018-10-12 14:53:42 +0200

    Added type hinting
    Type hinting added for clarity of information.

--- S907Reformat.py
+++ S907Reformat.py
@@ -1,12 +1,12 @@
 from easygui import fileopenbox, filesavebox
 
-file_open = fileopenbox()
+file_open: str = fileopenbox()
 with open(file_open, 'r') as file_r:
     lines = file_r.readlines()
 
 data: list = []
 for line in lines[::-1]:
-    word = line.split()
+    word: list = line.split()
 
     if len(word) < 9:
         word.insert(3, "  ")
@@ -17,7 +17,7 @@ for line in lines[::-1]:
 
 header_line = (f"DMDUNIT           SALEPLANCOHISTSTQTY          "
                "TSTARTDATPERIOD\n")
-file_save = filesavebox()
+file_save: str = filesavebox()
 with open(file_save, 'w') as file_w:
     file_w.write(header_line)
     for line in data:
